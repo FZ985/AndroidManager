fbba320ae9045691089b5cc38e4e73969ef8d080
飞舟 <945100985@qq.com>
2020-07-17 14:55:24 +0800

    创建管理库

---
+++ app/src/main/java/com/lxbuytimes/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.lxbuytimes.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.lxbuytimes.myapplication.activity.PasswordViewActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void passwordView(View view) {
+        startActivity(new Intent(this, PasswordViewActivity.class));
+    }
+}
---
+++ app/src/main/java/com/lxbuytimes/myapplication/activity/PasswordViewActivity.java
@@ -0,0 +1,43 @@
+package com.lxbuytimes.myapplication.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.lxbuytimes.myapplication.R;
+import com.lxbuytimes.myapplication.widget.IPasswordView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Create by JFZ
+ * date: 2020-07-17 14:24
+ **/
+public class PasswordViewActivity extends AppCompatActivity {
+    private IPasswordView pwd;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.passwordview_activity);
+        setTitle("密码控件");
+        pwd = findViewById(R.id.pwd);
+        pwd.setPasswordListener(new IPasswordView.PasswordListener() {
+            @Override
+            public void passwordChange(View view, String changeText) {
+
+            }
+
+            @Override
+            public void passwordComplete(View view) {
+                Toast.makeText(PasswordViewActivity.this, pwd.getPassword(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void keyEnterPress(View view, String password, boolean isComplete) {
+
+            }
+        });
+    }
+}
